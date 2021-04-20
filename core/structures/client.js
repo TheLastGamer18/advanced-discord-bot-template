c19c47514d61bd093da2a1656701ad128c73d9cd
TheLastGamer18 <61044552+TheLastGamer18@users.noreply.github.com>
2021-04-20 21:14:01 +0530

    Create client.js

---
+++ core/structures/client.js
@@ -0,0 +1,26 @@
+const { Client, Intents, Collection } = require("discord.js");
+
+class MusicClient extends Client {
+  
+  constructor() {
+    super({ ws: { intents: Intents.NON_PRIVILEGED } });
+    
+    this.config = require("../../config/config");
+    this.commands = new Collection();
+    this.aliases = new Collection();
+    
+  };
+  
+  run() {
+    
+   ["eventLoader", "commandLoader"].forEach(loader => {
+     require(`../../handlers/${loader}`)(this);
+   })
+    
+    this.login(this.config.token).catch(console.log);
+    
+  };
+  
+};
+
+module.exports = MusicClient;
