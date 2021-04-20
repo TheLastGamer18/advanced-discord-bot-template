716f32281048c484c24c3cf63329d79bb065b5ba
TheLastGamer18 <61044552+TheLastGamer18@users.noreply.github.com>
2021-04-20 20:57:34 +0530

    Update eventLoader.js

--- handlers/eventLoader.js
+++ handlers/eventLoader.js
@@ -1,1 +1,25 @@
+const fs = require("fs");
 
+module.exports = async(client) => {
+    
+  fs.readdirSync(`./events/`).forEach((dir) => {
+    
+    fs.readdir(`./events/${dir}/`, async (err, files) => {
+        if(err) {
+          console.log(`EventLoader Error: ${err}`);
+        };
+        
+        files.forEach(f => {
+          
+          let eventName = f.split(".")[0];
+          let event = require(`../events/${dir}/${f}`);
+          
+          client.on(eventName, event.bind(null, client));
+         
+      });
+    });
+  });
+  
+  console.log(`EventLoader: Loaded all events successfully!`);
+  
+};
